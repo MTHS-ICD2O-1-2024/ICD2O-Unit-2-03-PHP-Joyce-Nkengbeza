4626a0da0dfe00ac475e2d77f19dacead39d801c
default <Joyce.nkengbeza@stu.ocsb.ca>
2025-03-19 17:01:42 +0000

    address

--- answer.php
+++ answer.php
@@ -1,60 +1,62 @@
 <!DOCTYPE html>
 <!-- ICS2O-Unit1-08-HTML-MDL -->
 <html lang="en-ca">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="description" content="The XXX program, PHP" />
-    <meta name="keywords" content="mths, icd2o" />
-    <meta name="author" content="Mr. Coxall" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/icon?family=Material+Icons"
-    />
-    <link
-      rel="stylesheet"
-      href="https://code.getmdl.io/1.3.0/material.deep_purple-purple.min.css"
-    />
-    <link rel="stylesheet" href="./css/style.css" />
-    <link
-      rel="apple-touch-icon"
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="description" content="The Address with PHP" />
+  <meta name="keywords" content="mths, icd2o" />
+  <meta name="author" content="Joyce Nkengbeza" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <link
+    rel="stylesheet"
+    href="https://code.getmdl.io/1.3.0/material.deep_purple-purple.min.css" />
+  <link rel="stylesheet" href="./css/style.css" />
+  <link
+    rel="apple-touch-icon"
     sizes="180x180"
-    href="./apple-touch-iconnew.png"
-  />
+    href="./apple-touch-iconnew.png" />
   <link
     rel="icon"
     type="image/png"
     sizes="32x32"
-    href="./favicon-32x32new.png"
-  />
+    href="./favicon-32x32new.png" />
   <link
     rel="icon"
     type="image/png"
     sizes="16x16"
-    href="./favicon-16x16new.png"
-  />
-    <link rel="manifest" href="./site.webmanifest" />
-    <title>The XXX program, PHP</title>
-  </head>
+    href="./favicon-16x16new.png" />
+  <link rel="manifest" href="./site.webmanifest" />
+  <title>The Address in PHP</title>
+</head>
 
-  <body>
-    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-      <header class="mdl-layout__header">
-        <div class="mdl-layout__header-row">
-          <span class="mdl-layout-title">The XXX program, PHP</span>
+<body>
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">The Address With PHP</span>
+      </div>
+    </header>
+    <main class="mdl-layout__content">
+      <div class="right-image">
+        <img src="./images/notebook-animation.svg" alt="laptop image" width="250" />
+      </div>
+      <div class="page-content-php">
+        <div id="user-address">
+          <?php
+          $streetname = $_GET["streetname"];
+          $streetnumber = $_GET["streetnumber"];
+
+          echo "<p>Your info is: " . $streetname . ", age " . $streetnumber . ".</p>";
+          ?>
         </div>
-      </header>
-      <main class="mdl-layout__content">
-        <div class="right-image">
-          <img src="./images/cinnamoroll.jpeg" alt="Cinnamoroll" />
-        </div>
-        <div class="page-content">The answer is ...</div>
-        <br />
-        <div class="page-content-answer">
-          <a href="./index.php">Return ...</a>
-        </div>
-      </main>
-    </div>
-  </body>
+      </div>
+    </main>
+  </div>
+</body>
+
 </html>
--- index.php
+++ index.php
@@ -1,85 +1,76 @@
 <!DOCTYPE html>
 <!-- ICS2O-Unit2-03-HTML-MDL -->
 <html lang="en-ca">
-  <head>
+
+<head>
   <meta charset="utf-8" />
-  <meta name="description" content="The XXX program" />
+  <meta name="description" content="The Address With PHP" />
   <meta name="keywords" content="mths, icd2o" />
   <meta name="author" content="Joyce Nkengbeza" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link
     rel="stylesheet"
-    href="https://fonts.googleapis.com/icon?family=Material+Icons"
-  />
+    href="https://fonts.googleapis.com/icon?family=Material+Icons" />
   <link
     rel="stylesheet"
-    href="https://code.getmdl.io/1.3.0/material.deep_purple-purple.min.css"
-  />
+    href="https://code.getmdl.io/1.3.0/material.deep_purple-purple.min.css" />
   <link rel="stylesheet" href="./css/style.css" />
   <link
     rel="apple-touch-icon"
     sizes="180x180"
-    href="./apple-touch-iconnew.png"
-  />
+    href="./apple-touch-iconnew.png" />
   <link
     rel="icon"
     type="image/png"
     sizes="32x32"
-    href="./favicon-32x32new.png"
-  />
+    href="./favicon-32x32new.png" />
   <link
     rel="icon"
     type="image/png"
     sizes="16x16"
-    href="./favicon-16x16new.png"
-  />
+    href="./favicon-16x16new.png" />
   <link rel="manifest" href="./site.webmanifest" />
-  <title>The XXX program, PHP</title>
-  </head>
+  <title>The Address With PHP</title>
+</head>
 
 
-  <body>
-    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-      <header class="mdl-layout__header">
-        <div class="mdl-layout__header-row">
-          <span class="mdl-layout-title">The XXX program, PHP</span>
-        </div>
-      </header>
-      <main class="mdl-layout__content">
-        <div class="right-image">
-          <img src="./images/cinnamoroll.jpeg" alt="Cinnamoroll" />
-        </div>
-        <div class="page-content">Instructions here ...</div>
-        <br />
-        <!-- Simple Textfield for integers-->
+<body>
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">The Adress With PHP</span>
+      </div>
+    </header>
+    <main class="mdl-layout__content">
+      <div class="right-image">
+        <img src="./images/cinnamoroll.jpeg" alt="Cinnamoroll" />
+      </div>
+      <br />
+      <div class="page-content-php">
         <form action="answer.php" method="GET">
+          <p>Street Name</p>
           <div class="mdl-textfield mdl-js-textfield">
-            <!-- input pattern attribute -->
-            <input
-              class="mdl-textfield__input"
-              type="text"
-              pattern="-?[0-9]*(\.[0-9]+)?"
-              id="length"
-            />
-            <!-- mdl-textfield__label -->
-            <label class="mdl-textfield__label" for="demo-input"
-              >Length of ... (mm)</label
-            >
-            <!-- class "mdl-textfield__error" -->
-            <span class="mdl-textfield__error">Input is not a number</span>
+            <input class="mdl-textfield__input" type="text" name="streetname">
+            <label class="mdl-textfield__label" for="streetname-input">Street Name here ...</label>
+          </div>
+          <br />
+          <p>Street Number</p>
+          <div class="mdl-textfield mdl-js-textfield">
+            <input class="mdl-textfield__input" type=" text" pattern="-?[0-9]*(\.[0-9]+)?" name="streetnumber">
+            <label class="mdl-textfield__label" for="streetnumber-input">Street Number here ...</label>
+            <span class="mdl-textfield__error">Input is not a number!</span>
           </div>
           <br />
           <!-- Accent-colored raised button with ripple -->
-          <button
-            class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
-            type="submit"
-          >
-            Calculate
+          <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
+            type="submit">
+            Enter
           </button>
         </form>
-        <br />
-      </main>
-    </div>
-  </body>
+      </div>
+    </main>
+  </div>
+</body>
+
 </html>
